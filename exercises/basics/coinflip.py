d54f2666cd18952fe1a790722f4ec432a4671dd0
Eric Berglund <ericsamuelberglund@gmail.com>
2019-09-11 12:01:12 -0600

    Autopep8 files

--- exercises/basics/coinflip.py
+++ exercises/basics/coinflip.py
@@ -1,8 +1,10 @@
 from random import random
 
+
 def flip_coin():
     if random() > 0.5:
         return "HEADS"
     return "TAILS"
+
 
 print(flip_coin())
--- exercises/basics/converter.py
+++ exercises/basics/converter.py
@@ -1,5 +1,5 @@
 # Example input & output flow to convert kms to miles
 print("How many kilometers did you run today?")
 kms = float(input())
-miles =  round(kms / 1.609, 2)
+miles = round(kms / 1.609, 2)
 print(f"OK, you ran {miles} miles today!")
