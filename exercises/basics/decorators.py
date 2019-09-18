3598821e11c40fb941a4993ca0e6db4bd09dc3ff
Eric Berglund <ericsamuelberglund@gmail.com>
2019-09-18 13:49:15 -0600

    Additional decorator exercise

--- exercises/basics/decorators.py
+++ exercises/basics/decorators.py
@@ -1,6 +1,7 @@
 from time import time
 from functools import wraps
 
+
 def speed_test(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
@@ -11,13 +12,34 @@ def speed_test(fn):
         return result
     return wrapper
 
+
 @speed_test
 def sum_nums():
     return sum([x for x in range(20000)])
+
 
 @speed_test
 def sum_nums2():
     return sum(x for x in range(20000))
 
+
 print(sum_nums())
 print(sum_nums2())
+
+
+def ensure_no_kwargs(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if kwargs:
+            raise ValueError("No kwargs allowed!")
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+@ensure_no_kwargs
+def print_args(*args):
+    print(*args)
+
+
+print_args(1, 2, 3)
+print_args(1, 2, 3, asdf="asdf")
