c0bb218953efad04864061b20253ad84124d0cea
Eric Berglund <ericsamuelberglund@gmail.com>
2019-09-18 14:10:23 -0600

    Decorator with arg exercise

--- exercises/basics/decorators.py
+++ exercises/basics/decorators.py
@@ -23,8 +23,8 @@ def sum_nums2():
     return sum(x for x in range(20000))
 
 
-print(sum_nums())
-print(sum_nums2())
+# print(sum_nums())
+# print(sum_nums2())
 
 
 def ensure_no_kwargs(fn):
@@ -41,5 +41,24 @@ def print_args(*args):
     print(*args)
 
 
-print_args(1, 2, 3)
-print_args(1, 2, 3, asdf="asdf")
+# print_args(1, 2, 3)
+# print_args(1, 2, 3, asdf="asdf")
+
+# Example of passing args into decorator function
+def ensure_first_arg_is(val):
+    def inner(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            if args and args[0] != val:
+                return "First arg needs to be " + str(val)
+            return fn(*args, **kwargs)
+        return wrapper
+    return inner
+
+
+@ensure_first_arg_is("burrito")
+def fav_foods(*foods):
+    print(foods)
+
+print(fav_foods("burrito", "pizza"))
+print(fav_foods("pizza", "burrito"))
