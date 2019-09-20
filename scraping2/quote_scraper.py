ac64d71898279020353d7645b05a7da487cf164e
Eric Berglund <ericsamuelberglund@gmail.com>
2019-09-20 11:51:13 -0600

    Quote Scraping Project: Game Logic

--- scraping2/quote_scraper.py
+++ scraping2/quote_scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
+from random import choice
 
 base_url = "http://quotes.toscrape.com"
 url = f"{base_url}/page/1/"
@@ -19,6 +20,31 @@ while url:
     next_btn = soup.find(class_="next")
     url = f"{base_url}{next_btn.find('a')['href']}" if next_btn else None
     # Sleep to be polite scraping the website
-    sleep(2)
+    # sleep(2)
 
-print(all_quotes)
+quote = choice(all_quotes)
+print("Here's a quote: ")
+print(quote["text"])
+remaining_guesses = 4
+guess = ''
+print(quote["author"])
+while guess.lower() != quote["author"].lower() and remaining_guesses > 0:
+    guess = input(f"Who said this quote?: Guesses Remaining {remaining_guesses}\n")
+    if guess.lower() == quote['author'].lower():
+        print("YAY YOU WON!!!!")
+        break
+    remaining_guesses -= 1
+    if remaining_guesses == 3:
+        response = requests.get(f"{base_url}{quote['bio_link']}")
+        soup = BeautifulSoup(response.text, 'html.parser')
+        birthdate = soup.find(class_="author-born-date").get_text()
+        birthplace = soup.find(class_="author-born-location").get_text()
+        print(f"The author was born on {birthdate} {birthplace}")
+    elif remaining_guesses == 2:
+        print(f"The author's first name starts with {quote['author'][0]}")
+    elif remaining_guesses == 1:
+        lastname = quote["author"].split()[1]
+        print(f"The author's last name starts with {lastname[0]}")
+    else:
+        print("Sorry you ran out of guesses!")
+        print(f"The quote was from {quote['author']}")
